7a09b122243e84f6b3af2a8dc3821d8f5153b9dc
x-wasi <wasi.xwasi.wasix@gmail.com>
2025-09-10 14:16:15 +0500

    Create auth.js

---
+++ backend/routes/auth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const auth = async (req, res, next) => {
+  try {
+    const token = req.header('x-auth-token');
+    
+    if (!token) {
+      return res.status(401).json({ msg: 'No token, authorization denied' });
+    }
+    
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded.user;
+    next();
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
+
+module.exports = auth;
